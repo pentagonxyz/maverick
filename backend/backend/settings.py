35ccd3dc7b1338c79594140870c25db8fdd5696b
JetJadeja <JetJadeja@gmail.com>
2022-09-25 14:47:37 -0700

    Add postgres database to settings

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -76,8 +76,19 @@ WSGI_APPLICATION = 'backend.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+
+        'NAME': '',
+
+        'USER': '<db_username>',
+
+        'PASSWORD': '<password>',
+
+        'HOST': '<db_hostname_or_ip>',
+
+        'PORT': '<db_port>',
+
     }
 }
 
