76ce1bb3942a8499bcda77a1aee4f47b2c3ad707
JetJadeja <JetJadeja@gmail.com>
2022-09-26 20:35:24 -0700

    Implement Token Obtainer Serializer

---
+++ backend/maverick/serializers.py
@@ -0,0 +1,16 @@
+from django.contrib.auth.models import User
+from django.contrib.auth.password_validation import validate_password
+from rest_framework_simplejwt.serializers import TokenObtainPairSerializer
+from rest_framework import serializers
+from rest_framework.validators import UniqueValidator
+from rest_framework_simplejwt.serializers import TokenObtainPairSerializer
+
+class MyTokenObtainPairSerializer(TokenObtainPairSerializer):
+    @classmethod
+    def get_token(cls, user):
+        token = super().get_token(user)
+        # Add custom claims
+        token['username'] = user.username
+        token['email'] = user.email
+        # ...
+        return token
