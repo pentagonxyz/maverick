d60720807768b923fd2914239991ca6b05a627e0
JetJadeja <JetJadeja@gmail.com>
2022-09-26 20:49:03 -0700

    Rename API to Maverick

--- backend/backend/urls.py
+++ backend/backend/urls.py
@@ -4,5 +4,5 @@ from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('api/', include("api.urls"))
+    path('maverick/', include("maverick.urls"))
 ]
--- backend/maverick/views.py
+++ backend/maverick/views.py
@@ -2,7 +2,7 @@ from django.shortcuts import render
 from rest_framework.decorators import api_view
 from rest_framework.response import Response
 from django.http import JsonResponse
-from api.serializer import MyTokenObtainPairSerializer, RegisterSerializer
+from maverick.serializers import MyTokenObtainPairSerializer, RegisterSerializer
 from rest_framework_simplejwt.views import TokenObtainPairView
 from rest_framework import generics
 from django.contrib.auth.models import User
@@ -22,8 +22,8 @@ class RegisterView(generics.CreateAPIView):
 @api_view(['GET'])
 def getRoutes(request):
     routes = [
-        '/api/token/',
-        '/api/register/',
-        '/api/token/refresh/'
+        '/maverick/token/',
+        '/maverick/register/',
+        '/maverick/token/refresh/'
     ]
     return Response(routes)
