4023c3e80932594354fc1963fea3b8a6d8c3fc73
JetJadeja <JetJadeja@gmail.com>
2022-09-29 13:08:13 -0700

    Add additional fields to user model

--- backend/maverick/models.py
+++ backend/maverick/models.py
@@ -1,4 +1,6 @@
 from django.contrib.auth.models import User
+from django.db import models
 
 class MaverickUser(User):
-    pass
+    featured_image = models.ImageField(default = "default.png")
+    about = models.TextField(blank=True)
